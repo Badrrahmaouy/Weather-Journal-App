e0f7820fad9cddee9097a60c8b2484eb3727c1f0
Badr Rahmaouy <rahmaouy@gmail.com>
2021-02-27 19:32:34 +0100

    Weather JOurnal App 1.2.2

--- website/app.js
+++ website/app.js
@@ -1,6 +1,6 @@
 // Web API base URL and personal key
-let baseURL = 'https://api.openweathermap.org/data/2.5/weather?zip=';
-let apiKey = ',us&appid=6ad6f4e238882457dcb737040cf28fcb';
+const baseURL = 'https://api.openweathermap.org/data/2.5/weather?zip=';
+const apiKey = ',us&appid=6ad6f4e238882457dcb737040cf28fcb&units=imperial';
 
 // Event listener to add function to existing HTML DOM element
 document.getElementById('generate').addEventListener('click', makeForecast)
